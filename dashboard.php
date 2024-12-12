aac097610b62c7f37f2c355e4ff5bb6d644d44c6
Jose <jose@git.com>
2024-12-12 10:53:26 +0100

    putting sensitive data in .env

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,21 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
 
-require_once("vendor/autoload.php");
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 header('Access-Control-Allow-Origin: *'); 
 
 use Gri\Baloongame\Controller\QuestionController;
 use Gri\Baloongame\Controller\AnswerController;
 
+
 $controller = new QuestionController();
 $controller->showQuestions();
+
 $controller = new AnswerController();
 $controller->showAnswer();
 
--- db.php
+++ db.php
@@ -1,16 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ballongame";
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn->connect_error){
-    http_response_code(500); // Internal Server Error
+    http_response_code(500); 
     echo "Connection failed: " . $conn->connect_error;
 } else {
-    http_response_code(200); // OK
+    http_response_code(200);
     echo "Connection successful";
 }
 ?>
